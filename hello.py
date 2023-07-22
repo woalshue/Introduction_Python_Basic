b33038047b551642c9ef17b06d8e3d62fecf13fe
woalshue <133903914+woalshue@users.noreply.github.com>
2023-07-22 13:38:25 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -389,13 +389,263 @@ a = ['Life', 'is', 'too', 'short']
 result = " ".join(a) # a 리스트의 각 단어들을 한 문장으로 조립할 때 단어들 사이마다 공백을 넣어주어야 함. 1개의 공백문자 " " 를 사용하여 join
 print(result)
 
+
 # 튜플
-t1 = (1,2,'a', 'b')
-#del t1[0] # 리스트의 경우에는 0번째 요소인 1이 삭제되어야 할 텐데, 튜플의 경우에는 오류 발생. 'tuple' object doesn't support item deletion -> 삭제하는 걸 지원하지 않는다. 
-# 튜플은 리스트와는 다르게, 고정되어 있다.
-print(t1[0]) # 인덱스 볼 수는 있음
-print(t1[0:2]) # 슬라이싱도 가능. 그러나, t1 자체에는 변화 없음 
+
+# 리스트는 변경 가능 but 튜플은 변경 불가능
+# t1 = (1, 2, 'a', 'b')
+# del t1[0] -> 불가능. 리스트는 삭제할 수 있었는데, 튜플은 삭제도 불가능
+
+# 인덱싱 하기 
+t1 = (1, 2, 'a', 'b')
+print(t1[0])
+print(t1[3])
+
+# 슬라이싱 하기
+print(t1[1:]) # 1번째부터 끝까지
+
+# 튜플 더하기
+t2 = (3, 4)
+print(t1 + t2)
+
+# 튜플 곱하기
+print(t2 * 3) # 튜플에서 곱하기는 곧 '반복'이다. 3을 곱하면 t2가 세 번 반복된다. 
+
+# 튜플 길이 구하기
+print(len(t1)) # t1의 길이를 구해서 출력
+
+# 값 추가하기 ~ 
+t3 = (1, 2, 'a', 'b')
+print(t3 + (3,)) # t3 튜플에 3을 추가함 
+
+# 튜플은 요솟값을 변경할 수 없기 때문에 sort, insert, remove, pop 과 같은 내장함수가 없다. 
 
 
+# 딕셔너리 자료형
+dic = {'name' : 'jaemin', 'phone' : '010-6449-1625', 'birth' : '0219'}
+
+a = {1 : 'hi'} # key 로 정숫값 1, value 로 문자열 hi 를 사용함
+
+b = {'a' : [1, 2, 3]} # value 에 리스트도 넣을 수 있다
+
+# 딕셔너리 쌍 추가하기 
+a = {1 : 'a'}
+a[2] = 'b' # a 딕셔너리에 key가 2이고 value가 b인 딕셔너리 쌍을 추가
+print(a)
+
+a['name'] = 'pey'
+print(a)
+
+a[3] = [1, 2, 3] # 3인 key에 value로 리스트를 넣음
+print(a)
+
+# 딕셔너리 요소 삭제하기
+del a[1]
+print(a) # del a[key] 를 입력하면 key 에 해당하는 {key: value} 쌍이 삭제된다
+
+a = {"김연아": "피겨스케이팅", "류현진": "야구", "손흥민": "축구", "귀도": "파이썬"}
+print(a)
+
+
+# 딕셔너리에서 key를 사용해 value 얻기
+grade = {'pey': 10, 'julliet': 90}
+print(grade['pey']) # 'pey'라는 key의 value를 얻기 위해. 어떤 key 의 value 를 얻기 위해서는 딕셔너리변수이름[key] 를 사용해야 함. 
+
+a = {1: 'a', 2: 'b'}
+print(a[1])
+print(a[2])
+# 딕셔너리 변수에서 [] 안의 숫자 1은 두 번째 요소를 나타내는 것이 아니라 key에 해당하는 1을 나타낸다. 
+# 딕셔너리는 리스트나 튜플에 있는 인덱싱 방법을 적용할 수 없다. 
+
+my_dic = {'name': 'Jaemin', 'phone_number': '010-6449-1625', 'birth': '030219'}
+print(my_dic['name'])
+print(my_dic['phone_number'])
+print(my_dic['birth'])
+
+# 딕셔너리 만들 때 주의할 사항
+a = {1 : 'a', 1: 'b'}
+print(a)
+# >>> {1: 'b} 가 출력된다.
+# 딕셔너리에서 key는 고유한 값이므로 중복되는 key값을 설정해 놓으면 하나를 제외한 나머지 것들이 모두 무시된다는 점을 주의
+# 동일한 key가 2개 존재할 경우, 1: 'a' 쌍이 무시된다.
+# 즉, 딕셔너리에서는 동일한 key가 중복으로 존재할 수 없다.
+
+# 또 한 가지 주의해야 할 점 : key에 리스트는 쓸 수 없다는 것. (하지만 튜플은 key로 쓸 수 있다.)
+# 딕셔너리의 key로 쓸 수 있느냐, 없느냐는 key가 변하는 값인지, 변하지 않는 값인지에 달려 있다. 
+# 리스트는 그 값이 변할 수 있기 때문에 key로 쓸 수 없다. 
+# a = {[1,2]: 'hi'} # 리스트를 key로 설정하면 오류 발생.
+# 단, value에는 변하는 값이든, 변하지 않는 값이든 아무 값이나 넣을 수 있다. 
+
+
+# 딕셔너리 관련 함수 
+
+# key 리스트 만들기 - keys
+a = {'name': 'Jaemin', 'phone_number': '010-6449-1625', 'birth': '030219'}
+print(a.keys()) # a.keys()는 딕셔너리 a의 key만을 모아 dict_keys 객체를 리턴한다.
+# 만약 리턴값으로 리스트가 필요하다면, list(a.keys())를 사용하면 된다. (dict_keys 객체를 리스트로 변환)
+print(list(a.keys()))
+
+# dict_keys 객체 사용 (리스트를 사용하는 것과 별 차이는 없지만, 리스트 고유의 append, insert, pop, remove, sort 함수는 수행할 수 없다.)
+for k in a.keys():
+    print(k)
+
+# value 리스트 만들기 - values
+a = {'name': 'Jaemin', 'phone_number': '010-6449-1625', 'birth': '030219'}
+print(a.values()) # value만 얻고 싶다면 values 함수 사용. 호출 시 dict_values 객체를 리턴
+
+# key, value 쌍 얻기 - items
+a = {'name': 'Jaemin', 'phone_number': '010-6449-1625', 'birth': '030219'}
+print(a.items()) # items 함수는 key 와 value 의 쌍을 튜플로 묶은 값을 dict_items 객체로 리턴
+
+# key: value 쌍 모두 지우기 - clear
+a = {'name': 'Jaemin', 'phone_number': '010-6449-1625', 'birth': '030219'}
+a.clear() 
+print(a) # clear 함수는 딕셔너리 안의 모든 요소를 삭제. 빈 리스트를 [], 빈 튜플을 ()로 표현하는 것과 마찬가지로 빈 딕셔너리도 {}로 표현
+
+# key 로 value 얻기 - get
+a = {'name': 'Jaemin', 'phone_number': '010-6449-1625', 'birth': '030219'}
+print(a.get('name'))
+print(a.get('phone_number'))
+# get(x) 함수는 x라는 key에 대응되는 value를 리턴. 
+# a.get('name') 는 a['name'] 을 사용했을 때와 동일한 결과값을 리턴. 
+
+a = {'name': 'Jaemin', 'phone_number': '010-6449-1625', 'birth': '030219'}
+# print(a['nokey']) # a['nokey'] 방식을 사용하여 딕셔너리에 존재하지 않는 키로 값을 가져오려고 할 경우 에러를 발생시킴
+print(a.get('nokey')) # get 함수를 사용하여 딕셔너리에 존재하지 않는 키로 값을 가져오려고 할 경우 None 을 리턴 (None 은 '거짓' 이라는 뜻이라고만 알아 두기.)
+# 어떤 것을 사용할 것인지는 사용자의 선택. 
+
+# 딕셔너리 안에 찾으려는 키가 없을 경우, 미리 정해 둔 디폴트 값을 대신 가져오게 하고 싶을 때는 get(x, '디폴트 값') 을 사용하면 편리.
+print(a.get('nokey', 'Hello, Jaemin!')) # 딕셔너리 a 안에는 nokey 에 해당하는 키가 없으므로 설정해 둔 디폴트 값 Hello, Jaemin! 을 리턴
+
+# 해당 key가 딕셔너리 안에 있는지 조사하기 - in
+a = {'name': 'Jaemin', 'phone_number': '010-6449-1625', 'birth': '030219'}
+print('name' in a) # True 를 리턴
+print('email' in a) # False 를 리턴 
+
+
+# 집합 자료형 
+s1 = set([1, 2, 3]) # 집합 자료형(set 자료형)
+print(s1)
+print(type(s1)) # <class 'set'>
+ 
+s2 = {1, 2, 3} # 딕셔너리에서는 key가 꼭 있어야 했다면, 집합에서는 괄호 안에 키 - 밸류 없이 값들만 쭉 쓰면 그게 집합이 됨
+print(s2)
+print(type(s2))
+
+s1 = set("Hello")
+print(s1)
+# 집합에는 순서가 딱히 없기 때문에 랜덤하게 출력됨.
+# 집합에서 원소는 고유한 값이기 때문에 중복된 값들은 하나만 남고 제거됨
+# print(s1[0]) # 순서가 없기 때문에 인덱스로 접근하는 것 불가능
+
+s1 = set([1, 2, 3])
+l1 = list(s1) # 굳이 값을 가져오고 싶다면 list로 형 변환을 해 주어야 함
+print(l1[0])
+
+s1 = set([1, 2, 3])
+l1 = tuple(s1) # 튜플로도 형 변환 가능 
+print(l1)
+
+# 교집합
+s1 = set([1, 2, 3, 4, 5, 6])
+s2 = set([4, 5, 6, 7, 8, 9])
+print(s1 & s2) # 교집합 {4, 5, 6} 이 출력됨
+print(s1.intersection(s2)) # intersection 함수 사용 -> 집합1.intersection(집합2)를 사용히여 교집합 구하는 것도 가능
+
+# 합집합
+s1 = set([1, 2, 3, 4, 5, 6])
+s2 = set([4, 5, 6, 7, 8, 9])
+print(s1 | s2) # 합집합 {1, 2, 3, 4, 5, 6, 7, 8, 9} 이 출력됨
+print(s1.union(s2)) # union 함수 사용 -> 합집합 구할 수 있음
+
+# 차집합
+s1 = set([1, 2, 3, 4, 5, 6])
+s2 = set([4, 5, 6, 7, 8, 9])
+print(s1 - s2) # 차집합 {1, 2, 3} 이 출력됨
+# s1에서 s2를 빼는 것이므로, s1에서 s2와 중복된 것들을 뺴내고, s1에만 고유하게 존재하는 원소들을 뽑아내는 것
+print(s2 - s1) # {8, 9, 7} 가 출력됨
+print(s2.difference(s1)) # difference 함수 사용 -> 차집합 구할 수 있음
+
+# 집합 자료형 관련 함수
+# 값 1개 추가하기 - add (리스트에서는 append 였음)
+s1 = set([1, 2, 3])
+s1.add(4) # s1 집합에 4가 추가됨 (값을 한 개 추가)
+print(s1) 
+
+# s1.add([4, 5, 6]) -> 에러 발생. add는 원소 하나만 추가할 수 있는데, 리스트를 사용하여 여러 개를 추가하려고 하였기 때문
+
+
+# 값 여러 개 추가하기 - update
+s1 = set([1, 2, 3])
+s1.update([4, 5, 6]) # s1 집합에 4, 5, 6이 추가됨 (값을 여러개 추가)
+print(s1)
+
+s2 = set([1, 2, 3])
+s2.update([4, 4, 5, 6]) # 같은 원소를 중복하여 추가하려고 하면 중복은 제거되고 추가 됨 (리스트에서는 중복 모두 추가됨)
+print(s2)
+
+# 특정 값 제거하기 - remove
+s1 = set([1, 2, 3])
+s1.remove(2) # s1 집합 내에서 2 라는 원소를 제거
+# 여기서 2가 인덱스가 아니라, 실제 제거하고자 하는 값임. 집합에는 인덱스가 없음
+print(s1)
+
+# 언제 집합을 많이 활용하느냐? 
+l1 = [1, 2, 2, 3, 3, 3, 3, 4]
+# l1 리스트를 계속 리스트로 활용하고 싶은데, 고유한 값 하나씩만 남기고 싶을 떄
+s1 = list(set(l1)) # set 으로 l1을 감싸서 집합으로 형 변환을 시켜서 s1에 저장 -> 중복이 전부 제거됨 -> 다시 list로 감싸서 형 변환 
+print(s1) # 리스트 [1, 2, 3, 4] 출력됨 
+
+
+# 불 자료형
+a = True # a = "True" 와 같이 string 으로 적은 것과는 다름 
+print(a) # True 가 출력됨. 이것은 string "True" 와 다름
+print(type(a)) # type 을 찍어 보면 bool 자료형이라 알려줌 (불리언)
+
+a = False # False 도 마찬가지. 대신, 맨 앞이 대문자여야 함 (소문자 사용 시 에러 발생)
+print(a)
+
+a = 1 == 1 # 1과 1이 같니? -> True 
+print(a) # True가 출력됨
+
+a = 1 == 2
+print(a) # False 출력됨
+ 
+a = 1 < 2 
+print(a) # True
+
+# 자료형의 참과 거짓
+a = [1, 2, 3, 4]
+while a: # a 에 값이 있으므로 True -> 하나씩 날리다가 결국 a가 []이 되면 False 가 됨 ([]처럼 빈 상태는 False)
+    print(a)
+    a.pop() # pop 함수를 사용하여 끝 값을 하나씩 날림
+
+if []:
+    print("참")
+else:
+    print("거짓")
+# 위 조건문을 실행시켜 보면 "거짓" 이 출력됨. [] 리스트 안이 비어있기 때문에 False 이므로
+
+if [1, 2, 3]:
+    print("참")
+else:
+    print("거짓")
+# 위 조건문을 실행시키면 "참" 이 출력됨.
+
+# 불 연산
+a = bool([1, 2, 3]) # 값이 존재하는 리스트를 bool로 감싸주면 True 가 저장됨
+print(a)
+
+
+# 변수
+a = [1, 2, 3]
+print(id(a)) # id 함수는 주소를 보여줌
+
+a = [1, 2, 3] # a 변수 = '1, 2, 3이 담겨있는 리스트'(객체) -> a 변수에는 해당 객체의 '주소'가 할당되는 것임
+b = a # b 에 a 가 갖고 있는 주소를 동일하게 저당
+print(id(a))
+print(id(b))
+# a와 b의 주소를 출력해 보았을 때 값은 동일하게 출력될 수 밖에 없음
+print(a is b) # is 는 두 개의 주소가 같은지 판별하는 것 -> True 가 출력됨
 
 
